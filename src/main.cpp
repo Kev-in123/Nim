1a85e5b0c2b420a6f592f2a88cbed7d514fe7edb
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-10-31 09:34:57 -0400

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "nim.h"
+
+int main()
+{
+	Nim nim;
+    int in;
+    int ai;
+    std::cout << "Welcome to Nim!\n";
+    std::cout << "The person who takes the last stone loses\n\n";
+	while (nim.get_pile() > 0) {
+		std::cout << "There are " << nim.get_pile() << " stones in the pile\n";
+        std::cout << "How many stones would you like to take (1, 3, 4): "; std::cin >> in;
+        while (!nim.validEntry(in)) {
+            std::cout << "Invalid input\n";
+            std::cout << "How many stones would you like to take (1, 3, 4): "; std::cin >> in;
+        }
+        nim.remove_stone(in);
+	    if (nim.get_pile()  == 1) {
+            std::cout << "You win!";
+            return 0;
+        }
+        if (nim.get_pile() == 0) {
+            std::cout << "You lose!";
+            return 0;
+        }
+
+        ai = nim.ai_choice();
+        std::cout << "The AI took " << ai << " stones\n\n";
+        if (nim.get_pile() == 0) {
+            std::cout << "You win!";
+            return 0;
+        }
+    }
+    std::cout << "I suck at C++";
+    return 0;
+}
---
+++ src/nim.cpp
@@ -0,0 +1,34 @@
+ #include <random>
+#include "nim.h"
+
+int Nim::ai_choice() {
+	if ((this -> pile - 1) % 4 == 1) {
+		this -> pile -= 1;
+		return 1;
+	}
+	if ((this -> pile - 3) % 4 == 1) {
+		this -> pile -= 3;
+		return 3;
+	}
+	if ((this -> pile - 4) % 4 == 1) {
+		this -> pile -= 4;
+		return 4;
+	}
+
+	int n = this -> choices[(rand() * 3) + 1];
+	this -> pile -= n;
+	return n;
+}
+
+int Nim::get_pile() {
+	return this -> pile;
+}
+
+bool Nim::validEntry(int choice) {
+	return (choice == 1 || choice == 3 || choice == 4) && choice <= this -> pile;
+}
+
+void Nim::remove_stone(int n) {
+	this -> pile -= n;
+}
+
