809a2abc684ec85af41e7d53c4e61b8aad54dc75
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-14 09:02:24 -0500

    Update nim.h

--- src/nim.h
+++ src/nim.h
@@ -1,12 +1,12 @@
 #pragma once
 class Nim {
-	private:
-		int pile = 25;
-		int choices[3] = { 1, 3, 4 };
+private:
+	int pile = 25;
+	int choices[3] = { 1, 3, 4 };
 
-	public:
-		int ai_choice();
-		int get_pile();
-		bool validEntry(int choice);
-		void remove_stone(int n);
+public:
+	int ai_choice();
+	int get_pile();
+	bool validEntry(int choice);
+	void remove_stone(int n);
 };
