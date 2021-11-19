03907b023887b6e5385ec08e49df330ce9fa74e7
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-18 22:47:39 -0500

    Update nim.h

--- src/nim.h
+++ src/nim.h
@@ -3,10 +3,10 @@
 
 class Nim {
 private:
-	int pile = 18 + rand() % 6;
+    int pile = 18 + rand() % 6;
 public:
-	int ai_choice();
-	int get_pile();
-	bool validEntry(int choice);
-	void remove_stone(int n);
+    int ai_choice();
+    int get_pile();
+    bool validEntry(int choice);
+    void remove_stone(int n);
 };
