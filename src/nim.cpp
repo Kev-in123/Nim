8dbde16ffecaeaf3b5be4f1734f1ebdc703a883a
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-18 22:47:20 -0500

    Update nim.cpp

--- src/nim.cpp
+++ src/nim.cpp
@@ -2,26 +2,26 @@
 #include "nim.h"
 
 int Nim::ai_choice() {
-	for (int i = 1; i < 4; i++) {
-		if ((pile - i) % 4 == 1) {
-			return i;
-		}
+    for (int i = 1; i < 4; i++) {
+	if ((pile - i) % 4 == 1) {
+	    return i;
 	}
-	int n = (rand() % 3) + 1;
-	while (!validEntry(n)) {
-		n = (rand() % 3) + 1;
-	}
-	return n;
+    }
+    int n = (rand() % 3) + 1;
+    while (!validEntry(n)) {
+	n = (rand() % 3) + 1;
+    }
+    return n;
 }
 
 int Nim::get_pile() {
-	return this->pile;
+    return this->pile;
 }
 
 bool Nim::validEntry(int choice) {
-	return (1 <= choice && choice <= 3) && choice <= this->pile;
+    return (1 <= choice && choice <= 3) && choice <= this->pile;
 }
 
 void Nim::remove_stone(int n) {
-	this->pile -= n;
+    this->pile -= n;
 }
